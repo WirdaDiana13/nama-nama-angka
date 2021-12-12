b5e34cfc0d232de0507580441c14bafad1d0eacf
wirda diana nesywa <92450175+WirdaDiana13@users.noreply.github.com>
2021-12-12 13:46:36 +0700

    Add files via upload

---
+++ Postest 3 nesy.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Wirda Diana Nesywa
+// 2117051059
+// A
+// Postest 3
+
+int main (){
+	
+	int Nama;
+	
+	cout<<"Nama Nama Angka"<<endl;
+	cout<<"Masukkan Angka :";
+	cin>>Nama;
+	
+	if (Nama == 1){
+		cout<<"Satu"<<endl;
+	}else if (Nama == 2){
+		cout<<"Dua"<<endl;
+	}else if (Nama == 3){
+		cout<<"Tiga"<<endl;
+	}else if (Nama == 4){
+		cout<<"Empat"<<endl;
+	}else if (Nama == 5){
+		cout<<"Lima"<<endl;
+	}else if (Nama == 6){
+		cout<<"Enam"<<endl;
+	}else if (Nama == 7){
+		cout<<"Tujuh"<<endl;
+	}else if (Nama == 8){
+		cout<<"Delapan"<<endl;
+	}else if (Nama == 9){
+		cout<<"Sembilan"<<endl;
+	}else{
+		cout<<Nama<<endl;
+	}
+	
+	return 0;	
+}
